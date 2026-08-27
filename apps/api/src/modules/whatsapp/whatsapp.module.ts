b7efab05b0f8179cac759972443060edf9dfa5f2
3omarmagdy <omarelbendary51@gmail.com>
2026-08-27 22:29:43 +0000

    fix(whatsapp): import audit module

--- apps/api/src/modules/whatsapp/whatsapp.module.ts
+++ apps/api/src/modules/whatsapp/whatsapp.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { AuditModule } from '../audit/audit.module';
 import { WhatsAppController } from './whatsapp.controller';
 import { WhatsAppMarketingService } from './whatsapp-marketing.service';
 import { WhatsAppService } from './whatsapp.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, AuditModule],
   controllers: [WhatsAppController],
   providers: [WhatsAppService, WhatsAppMarketingService],
   exports: [WhatsAppService, WhatsAppMarketingService],
